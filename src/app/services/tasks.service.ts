cd843d57d9b5bf6e34c85ea6b81066d18717418b
Nagendra Patil <87421834+NagendraPatil@users.noreply.github.com>
2025-12-18 23:52:26 +0530

    added a logging service

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from "@angular/core";
+import { Task, TaskStatus } from "../tasks/task.model";
+import { LoggingService } from "./logging.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class TasksService {
+  private tasks = signal<Task[]>([]);
+  private loggingService = inject(LoggingService);
 
-  constructor() { }
+  allTasks = this.tasks.asReadonly();
+
+  constructor() {}
+
+  addTask(task: { title: string; description: string }) {
+    const newTask: Task = {
+      id: Math.random().toString(36).substring(2, 15),
+      title: task.title,
+      description: task.description,
+      status: "OPEN",
+    };
+    this.tasks.update((tasks) => [...tasks, newTask]);
+    this.loggingService.log(`Task added: ${newTask.title}`);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) => {
+        return task.id === taskId ? { ...task, status: newStatus } : task;
+      })
+    );
+    this.loggingService.log(`Task status updated: ${taskId} to ${newStatus}`);
+  }
 }
--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,17 +1,36 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from "@angular/core";
 
-import { TaskItemComponent } from './task-item/task-item.component';
+import { TaskItemComponent } from "./task-item/task-item.component";
+import { TasksService } from "../../services/tasks.service";
 
 @Component({
-  selector: 'app-tasks-list',
+  selector: "app-tasks-list",
   standalone: true,
-  templateUrl: './tasks-list.component.html',
-  styleUrl: './tasks-list.component.css',
+  templateUrl: "./tasks-list.component.html",
+  styleUrl: "./tasks-list.component.css",
   imports: [TaskItemComponent],
 })
 export class TasksListComponent {
-  selectedFilter = signal<string>('all');
-  tasks = [];
+  private selectedFilter = signal<string>("all");
+  private taskService = inject(TasksService);
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case "open":
+        return this.taskService
+          .allTasks()
+          .filter((task) => task.status === "OPEN");
+      case "done":
+        return this.taskService
+          .allTasks()
+          .filter((task) => task.status === "DONE");
+      case "in-progress":
+        return this.taskService
+          .allTasks()
+          .filter((task) => task.status === "IN_PROGRESS");
+      default:
+        return this.taskService.allTasks();
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
